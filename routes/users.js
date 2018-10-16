6fae50f0fddfa6efc57c639dc2a43b7d25cf869c
HellocsWorldgit config --global user.email antonio.94124@gmail.comssh-keygen -t rsa -C antonio.94124@gmail.com <rserran2@mail.sfsu.edu>
2018-10-15 19:43:36 -0700

    local version
    none

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ const router = express.Router()
 
 router.get("/users", (req, res) => {
     const connection = getConnection()
-    const queryString = "SELECT * From users"
+    const queryString = "SELECT * From user_in"
     connection.query(queryString, (err, rows, fields) => {
         if(err){
             console.log("Failed to query for users: " + err)
@@ -47,7 +47,7 @@ router.get('/user/:id', (req, res) => {
     const connection = getConnection()
 
     const userId = req.params.id
-    const queryString = "SELECT * FROM users WHERE id = ?"
+    const queryString = "SELECT * FROM user_in WHERE user_id = ?"
     connection.query(queryString, [userId], (err, rows, fields) => {
        if(err) {
           console.log("failed to query for users " + err)
@@ -56,20 +56,27 @@ router.get('/user/:id', (req, res) => {
        }
         console.log("I think we got users")
         const users = rows.map((row) => {
-            return {first: row.firstName, last: row.lastName}
+            return {first: row.first_Name, last: row.last_Name, ID: row.student_id, userName: row.userName,}
         })
+        //you can display everything by using the following line of code
+        //res.json(rows)
+        //or display custom by using const users above and then using 
         res.json(users)
     })
 })
+
 
 router.post('/user_create', (req, res) => {
     console.log("getting user input")
     console.log("First name: " + req.body.create_first_name)
     const firstName = req.body.create_first_name
     const lastName = req.body.create_last_name
+    const studentID = req.body.create_student_id
+    const userName = req.body.create_username
+    const password = req.body.create_psw
     
-    const queryString = "INSERT INTO users (firstName, lastName) VALUES (?, ?)"
-    getConnection().query(queryString, [firstName, lastName], (err, results, fields) => {
+    const queryString = "INSERT INTO user_in (first_Name, last_Name, student_id, userName, user_password) VALUES (?, ?, ?, ?, ?)"
+    getConnection().query(queryString, [firstName, lastName, studentID, userName, password], (err, results, fields) => {
         if (err) {
             console.log("failed to insert new user: " + err)
             res.sendStatus(500)
